5fbce4a470837807a8235d23998d5375f17dc781
mmotorwala <111542130+mmotorwala@users.noreply.github.com>
2024-01-03 19:46:12 +0530

    Update post_fhir.sh

--- post_fhir.sh
+++ post_fhir.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 echo "Starting to post JSON bundles - "
-path="/usr/src/synthea/output/fhir/*.json"  # Adjusted the path to the container's directory
-URL="http://host.docker.internal:8000/"  # Assuming this is your FHIR server URL
-
+path="/usr/src/synthea/output/fhir/*.json"  # Reading Synthea FHIR bundles from /output/fhir directory
+URL="http://host.docker.internal:8000/"  # Assuming FHIR server is running on localhost:8000
+cred="YWRtaW46cGFzc3dvcmQ=" #base64 encoded creds
+counter=0;
 for f in $path; do 
     if [ -f "$f" ]; then  # Checking if it's a file
         resource_count=$(jq '.entry | length' $f)
         echo "Posting file: $f with $resource_count resources"
-        { time curl -X POST -H "Content-Type: application/json" -H 'Authorization: Basic YWRtaW46cGFzc3dvcmQ=' \
+        { time curl -X POST -H "Content-Type: application/json" -H 'Authorization: Basic '$cred'' \
           $URL \
           -d @$f; } 2>&1 | sed -n 's/real/Time:/p'
+          let counter++
           echo
     fi 
 done
+echo "Posted $counter Bundles in total."
