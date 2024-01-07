134f3168abadcda69f28ff8a937014664c2305d3
mmotorwala <111542130+mmotorwala@users.noreply.github.com>
2024-01-07 16:44:57 +0530

    Update post_fhir.sh

--- post_fhir.sh
+++ post_fhir.sh
@@ -1,14 +1,21 @@
 #!/bin/bash
+#! created by Murtuza Motorwala
+echo "Starting the utility"
+echo "Enter your FHIR server's Base URL. If the server is running locally on docker then use - http://host.docker.internal:8000/"
+read URL
+echo "Enter the username"
+read user
+echo "Enter the password"
+read password
 echo "Starting to post JSON bundles - "
-path="/usr/src/synthea/output/fhir/*.json"  # Reading Synthea FHIR bundles from /output/fhir directory
-URL="http://host.docker.internal:8000/"  # Assuming FHIR server is running on localhost:8000
-cred="YWRtaW46cGFzc3dvcmQ=" #base64 encoded creds
+path="/usr/src/synthea/output/fhir/*.json"  #Reading Synthea FHIR bundles from /output/fhir directory
 counter=0;
 for f in $path; do 
     if [ -f "$f" ]; then  # Checking if it's a file
         resource_count=$(jq '.entry | length' $f)
-        echo "Posting file: $f with $resource_count resources"
-        { time curl -X POST -H "Content-Type: application/json" -H 'Authorization: Basic '$cred'' \
+        size=$(ls -lh $f | awk '{print "size:", $5}')
+        echo "Posting file: $f with $resource_count resources and $size"
+        { time curl -X POST -H "Content-Type: application/json" -u $user:$password \
           $URL \
           -d @$f; } 2>&1 | sed -n 's/real/Time:/p'
           let counter++
